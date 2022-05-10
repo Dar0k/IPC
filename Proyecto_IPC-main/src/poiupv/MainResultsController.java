fe4ebbbd285b6349e0917945559f824c9c0fcd3e
raulojeda22 <raulojeda10g@gmail.com>
2022-05-10 14:12:04 +0200

    Show results added

--- Proyecto_IPC-main/src/poiupv/MainResultsController.java
+++ Proyecto_IPC-main/src/poiupv/MainResultsController.java
@@ -5,8 +5,11 @@
  */
 package poiupv;
 
+import DBAccess.NavegacionDAOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,6 +21,15 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.User;
+import model.Session;
+import model.Navegacion;
+import javafx.scene.control.DatePicker;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -30,6 +42,8 @@ public class MainResultsController implements Initializable {
     private Scene prevScene;
     private String prevTitle;
     
+    
+    
     @FXML
     private Button continueButton;
     @FXML
@@ -40,8 +54,21 @@ public class MainResultsController implements Initializable {
     private VBox sidebar;
     @FXML
     private SidebarController sidebarController;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TableView<Session> tableResults;
+    @FXML
+    public TableColumn<Session, Integer> hits;
+    @FXML
+    public TableColumn<Session, Integer> faults;
+    @FXML
+    public TableColumn<Session, LocalDateTime> timestamp;
 
     User user;
+    Session session;
+    Navegacion navegation;
+    
     /**
      * Initializes the controller class.
      */
@@ -50,21 +77,46 @@ public class MainResultsController implements Initializable {
         // TODO
         sidebarController.initialize(url, rb);
         updateSidebar();
+        setupTable();
     }
     
     public void initStage(Stage stage, User us)
     {
+        try {            
+            navegation = Navegacion.getSingletonNavegacion();
+        } catch (NavegacionDAOException ex) {
+            Logger.getLogger(LogInDefController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         primaryStage = stage;
         primaryStage.setTitle("Results");
         sidebarController.primaryStage = primaryStage;
         user = us;
         sidebarController.setUser(user);
+        loadResults();
     }
     
     public void updateSidebar()
     {
         sidebarController.clearSidebar();
         sidebarController.boldResultsButton();
+    }
+    
+    private void setupTable()
+    {
+        hits.setCellValueFactory(new PropertyValueFactory<>("hits"));
+        faults.setCellValueFactory(new PropertyValueFactory<>("faults"));
+        timestamp.setCellValueFactory(new PropertyValueFactory<>("timeStamp"));
+    }
+    
+    public void loadResults()
+    {
+        tableResults.getItems().clear();
+        List<Session> sessions = user.getSessions();
+        for (Session sess: sessions) {
+            if ((datePicker.getValue() != null && sess.getLocalDate().isAfter(datePicker.getValue())) || datePicker.getValue() == null) {
+                tableResults.getItems().add(sess);
+            }
+        }
     }
     
 
